e9ee78dc68a526b29cb90a0596e5ed4f84064281
milkiascha7 <mikehailu504@gmail.com>
2024-06-28 17:10:24 -0700

    "rename folder error"

--- components/Trustedby.tsx
+++ components/Trustedby.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useRef } from "react";
-import ClientLogos from "./Client-logos";
+import ClientLogos from "./ClientLogos";
 import styles from "../styles/trusted.module.css";
 import { delay, easeInOut, motion, useInView } from "framer-motion";
 
